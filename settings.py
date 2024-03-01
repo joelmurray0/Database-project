55c10d46e77368e27282aa9f601155d339aee1af
joelmurray0 <joel@moonbug.co.uk>
2024-03-01 11:36:47 +0000

    basic info/test

---
+++ settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
---
+++ urls.py
@@ -0,0 +1,8 @@
+# urls.py
+
+from django.urls import path
+from .views import post_list
+
+urlpatterns = [
+    path('posts/', post_list, name='post_list'),
+]
